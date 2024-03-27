0d10e00ccea37a4c237f159e873f47af1d15550f
LobJosias22 <144252897+LobJosias22@users.noreply.github.com>
2024-03-27 07:51:02 +0000

    fin migration

--- db_scripts/migrate_1.sql
+++ db_scripts/migrate_1.sql
@@ -1,1 +1,63 @@
 ALTER TABLE singer RENAME TO musician;
+
+ALTER TABLE musician CHANGE COLUMN singerName musicianName VARCHAR(50);
+
+
+CREATE TABLE band (
+    bandName VARCHAR(50),
+    creation YEAR,
+    genre VARCHAR(50),
+    PRIMARY KEY (bandName)
+);
+INSERT INTO band (bandName, creation, genre)
+VALUES
+    ('Crazy Duo', 2015, 'rock'),
+    ('Luna', 2009, 'classical'),
+    ('Mysterio', 2019, 'pop');
+
+
+ALTER TABLE musician
+ADD COLUMN role VARCHAR(50);
+
+ALTER TABLE musician
+ADD COLUMN bandName VARCHAR(50);
+
+
+UPDATE musician
+SET role = 'vocals'
+WHERE musicianName = 'Alina';
+
+
+UPDATE musician
+SET role = 'guitar'
+WHERE musicianName = 'Mysterio';
+
+
+UPDATE musician
+SET role = 'percussion'
+WHERE musicianName = 'Rainbow';
+
+
+UPDATE musician
+SET role = 'piano'
+WHERE musicianName = 'Luna';
+
+
+UPDATE musician
+SET bandName = 'Crazy Duo'
+WHERE musicianName = 'Alina';
+
+
+UPDATE musician
+SET bandName = 'Mysterio'
+WHERE musicianName = 'Mysterio';
+
+
+UPDATE musician
+SET bandName = 'Crazy Duo'
+WHERE musicianName = 'Rainbow';
+
+
+UPDATE musician
+SET bandName = 'Luna'
+WHERE musicianName = 'Luna';
--- db_scripts/rollback_1.sql
+++ db_scripts/rollback_1.sql
@@ -1,3 +1,9 @@
-BEGIN;
-ROLLBACK;
-COMMIT;
+ALTER TABLE musician RENAME TO singer;
+
+ALTER TABLE singer CHANGE COLUMN musicianName singerName VARCHAR(50);
+
+DROP TABLE band;
+
+ALTER TABLE singer DROP COLUMN role;
+
+ALTER TABLE singer DROP COLUMN bandName;
